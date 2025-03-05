65e903b8565e55698e7ad63eb344993f1607754a
Kristin Salvacion <nitnitks@gmail.com>
2025-03-04 19:42:25 -0500

    Allowed for page navigation from first screen to log-in and sign-up, created log-in and sign-up pages with text fields (currently the text changes for all inputs with the text the user types this will be changed)

---
+++ app/register.jsx
@@ -0,0 +1,98 @@
+import { StyleSheet, Text, View, ImageBackground, TextInput, TouchableOpacity} from 'react-native'
+import React, {useState} from 'react'
+import appBgImg from "@/assets/images/appBg.png"
+import logo from "@/assets/images/logo.png"
+
+const register = () => {
+    const [text, setText] = useState('');
+  return (
+    <View style = {styles.container} >
+        <ImageBackground
+                source = {appBgImg}
+                resizeMode = "cover"
+                style = {styles.image}
+        >
+
+        <Text style = {styles.text}>Sign-Up</Text>
+        <TextInput
+            style = {styles.input}
+            placeholder = "First Name"
+            onChangeText = {newText => setText(newText)}
+            defaultValue = {text}
+        />
+         <TextInput
+            style = {styles.input}
+            placeholder = "Last Name"
+            onChangeText = {newText => setText(newText)}
+            defaultValue = {text}
+        />
+        <TextInput
+            style = {styles.input}
+            placeholder = "E-mail"
+            onChangeText = {newText => setText(newText)}
+            defaultValue = {text}
+        />
+        <TextInput
+            style = {styles.input}
+            placeholder = "Username"
+            onChangeText = {newText => setText(newText)}
+            defaultValue = {text}
+        />
+        <TextInput
+            style = {styles.input}
+            placeholder = "Password"
+            onChangeText = {newText => setText(newText)}
+            defaultValue = {text}
+        />
+        <TextInput
+            style = {styles.input}
+            placeholder = "Confirm Password"
+            onChangeText = {newText => setText(newText)}
+            defaultValue = {text}
+        />
+        <TouchableOpacity style={{ backgroundColor: '#261605', padding: 10, borderRadius: 15, marginHorizontal: 30, marginVertical: 7}}>
+            <Text style={{ color: '#fff', textAlign: 'center', fontWeight: 'bold' }}>Submit</Text>
+        </TouchableOpacity>
+        </ImageBackground>
+    </View>
+  )
+}
+
+export default register
+
+const styles = StyleSheet.create({
+    container: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      flex: 1, 
+      flexDirection: 'column', 
+      marginHorizontal: 16,
+    },
+    image:{
+      width: '100%', 
+      height: '100%',
+      flex: 1, 
+      resizeMode: 'cover',
+      justifyContent: 'center',
+    },
+    text: {
+      color: '#261605',
+      fontSize: 42, 
+      fontWeight: 'bold', 
+      textAlign: 'center',
+      marginVertical: 10,
+    },
+    input:{
+        backgroundColor: "#FFF3D8",
+        height: 40,
+        marginVertical: 7, 
+        marginHorizontal: 30,
+        borderWidth: 2, 
+        padding:10,
+        borderRadius: 15,
+    },
+    space: {
+      width: 20, 
+      height: 20,
+    },
+  })
